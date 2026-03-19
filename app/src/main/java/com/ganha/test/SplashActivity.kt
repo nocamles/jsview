b88358c1d9b6e17a52b6fbee22383862d03ebc22
yanghong <cs16xiaoc1@163.com>
2026-03-19 14:02:23 +0800

    splash页面添加。

---
+++ app/src/main/java/com/ganha/test/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.ganha.test
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // 设置延迟 1.5 秒跳转到 MainActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1500)
+    }
+}
