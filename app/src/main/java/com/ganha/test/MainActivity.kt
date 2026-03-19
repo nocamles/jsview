8eff8fdd0815179296f44a6f5b816fd477cb0a86
Aike-Deng <97423215@qq.com>
2026-03-19 21:45:07 +0800

    1.集成Firebase和RemoteConfigSDK，已调试通过；

--- app/src/main/java/com/ganha/test/MainActivity.kt
+++ app/src/main/java/com/ganha/test/MainActivity.kt
@@ -88,12 +88,21 @@ import java.io.FileOutputStream
 import java.io.ByteArrayOutputStream
 import android.webkit.ValueCallback
 import android.app.Activity
+import android.util.Log
 import com.ganha.test.bean.AppUpdateBean
 import com.ganha.test.bean.AppinfoBean
 import com.ganha.test.bean.JsBean.Companion.js_deviceInfo
 import com.ganha.test.bean.JsBean.Companion.js_saveImageToGallery
 import com.ganha.test.utils.DeviceIdUtil
 import com.ganha.test.utils.DeviceInfoHelper
+import com.google.firebase.Firebase
+import com.google.firebase.remoteconfig.ConfigUpdate
+import com.google.firebase.remoteconfig.ConfigUpdateListener
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigException
+import com.google.firebase.remoteconfig.get
+import com.google.firebase.remoteconfig.remoteConfig
+import com.google.firebase.remoteconfig.remoteConfigSettings
 
 class MainActivity : AppCompatActivity() {
 
@@ -230,6 +239,8 @@ class MainActivity : AppCompatActivity() {
         checkAndClearDownloadCache()
         splash_webview?.loadUrl("file:///android_asset/splash_screen.html")
         webView.loadUrl("file:///android_asset/myTest.html")
+
+        initFbConfig()
     }
 
     private fun checkAndClearDownloadCache() {
@@ -1253,5 +1264,64 @@ class MainActivity : AppCompatActivity() {
                 ).show()
             }
         }
+    }
+
+    private val TAG = "FbConfig"
+
+    private fun initFbConfig(){
+        // [START get_remote_config_instance]
+        val remoteConfig: FirebaseRemoteConfig = Firebase.remoteConfig
+        // [END get_remote_config_instance]
+
+        // [START enable_dev_mode]
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 3600
+        }
+        remoteConfig.setConfigSettingsAsync(configSettings)
+        // [END enable_dev_mode]
+
+        // [START set_default_values]
+        remoteConfig.setDefaultsAsync(R.xml.remote_config_defaults)
+        // [END set_default_values]
+
+        // [START fetch_config_with_callback]
+        remoteConfig.fetchAndActivate()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val updated = task.result
+                    Log.d(TAG, "Config params updated: $updated")
+                } else {
+                    Log.d(TAG, "Fetch failed")
+                }
+                displayWelcomeMessage()
+            }
+        // [END fetch_config_with_callback]
+
+        // [START add_config_update_listener]
+        remoteConfig.addOnConfigUpdateListener(object : ConfigUpdateListener {
+            override fun onUpdate(configUpdate: ConfigUpdate) {
+                Log.d(TAG, "Updated keys: " + configUpdate.updatedKeys)
+
+                if (configUpdate.updatedKeys.contains("welcome_message")) {
+                    remoteConfig.activate().addOnCompleteListener {
+                        displayWelcomeMessage()
+                    }
+                }
+            }
+
+            override fun onError(error: FirebaseRemoteConfigException) {
+                Log.w(TAG, "Config update error with code: " + error.code, error)
+            }
+        })
+        // [END add_config_update_listener]
+    }
+
+    private fun displayWelcomeMessage() {
+        val remoteConfig = Firebase.remoteConfig
+        // [START get_config_values]
+        val mainUrlText = remoteConfig["main_url_text"].asString()
+        val mainUrlGanha = remoteConfig["main_url_ganha"].asString()
+        Log.d(TAG,"mainUrlText:${mainUrlText}\nmainUrlGanha:${mainUrlGanha}")
+        // [END get_config_values]
     }
 }
